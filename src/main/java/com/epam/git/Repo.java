25dd0acb8550527310007a0671bc711ac5a2e573
AliNajimov <najimovm11@gmail.com>
2023-05-15 13:59:14 +0500

    .

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,6 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/AliNajimov/stage2-module1-git";
-
+    public static String REPO_LINK = "https://github.com/TaBletKa97/stage2-module1-git";
 }
